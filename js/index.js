dc073676a8cab2fa71df74a1c36d1b5af42223bf
Chad Lajeunesse <lajeunesse.chad@gmail.com>
2014-03-26 22:11:37 -0400

    create tables works 100%

--- js/index.js
+++ js/index.js
@@ -37,7 +37,7 @@ function checkDB(){
 				//otherwise your new version will be an empty DB
 				alert("DB version incremented");
 				//do the inition setup
-				trans.executeSql('CREATE TABLE IF NOT EXISTS destinations (destination_id INTEGER UNSIGNED NOT NULL AUTOINCREMENT, tour_id INTEGER UNSIGNED NOT NULL, seq_num INTEGER UNSIGNED NOT NULL, destination_name TEXT NOT NULL, destination_description TEXT NOT NULL, latitude TEXT NOT NULL, longitude TEXT NOT NULL, destination_image TEXT NOT NULL, image_mime TEXT NOT NULL, PRIMARY KEY(destination_id))', [],
+				trans.executeSql('CREATE TABLE IF NOT EXISTS destinations (destination_id INTEGER PRIMARY KEY AUTOINCREMENT, tour_id INTEGER UNSIGNED NOT NULL, seq_num INTEGER UNSIGNED NOT NULL, destination_name TEXT NOT NULL, destination_description TEXT NOT NULL, latitude TEXT NOT NULL, longitude TEXT NOT NULL, destination_image TEXT NOT NULL, image_mime TEXT NOT NULL)', [],
 					function(tx, rs){
 						//do something if it works
 						alert("Table destinations created");
@@ -46,7 +46,7 @@ function checkDB(){
 						//failed to run query
 						alert("YES destinations err " + err.message);
 					});
-				trans.executeSql('CREATE TABLE IF NOT EXISTS tours (tour_id INTEGER UNSIGNED NOT NULL AUTOINCREMENT, tour_name TEXT NOT NULL, tour_description TEXT NOT NULL, tour_image TEXT NOT NULL, active INTEGER NOT NULL, PRIMARY KEY(tour_id))', [],
+				trans.executeSql('CREATE TABLE IF NOT EXISTS tours (tour_id INTEGER PRIMARY KEY AUTOINCREMENT, tour_name TEXT NOT NULL, tour_description TEXT NOT NULL, tour_image TEXT NOT NULL, active INTEGER NOT NULL)', [],
 					function(tx, rs){
 						//do something if it works
 						alert("Table tour created");
